1a568b9972db2f73d1bceaea7e9d264500ff365b
paulopreto <paulosantiago@usp.br>
2021-09-27 11:55:45 -0300

    encotro06 fim hoje

--- Encontro06/importc3d.py
+++ Encontro06/importc3d.py
@@ -4,11 +4,12 @@ Created on Sat Jun 13 08:35:07 2020
 
 @author: preto
 """
+import matplotlib.pyplot as plt
 from ezc3d import c3d
 import sys
 
 def importc3d(dat=None):
-    # dat = input('Qual o c3d? ')
+    # dat = input('Qual o c3d voce quer carregar? ')
     # dat = 'Mayara05.c3d'
     c = c3d(dat)
     print('\nNumber of markers = ', c['parameters']['POINT']['USED']['value'][0])  # Print the number of points used
@@ -23,5 +24,15 @@ def importc3d(dat=None):
     
     return markers, labels, analog_data, freq3d, freqforce 
 
+# markers, labels, analog_data, freq3d, freqforce = importc3d()
+
 if __name__ == '__main__':
-    importc3d(str(sys.argv[1]))
+    markers, labels, analog_data, freq3d, freqforc = importc3d(str(sys.argv[1]))
+    marker01 = markers[:, 0, :]
+    marker01_z = markers[:, 0, 2]
+    # Pegar o valor do maxio em mm e nao a posicao do maximo
+    plt.plot(marker01_z)
+    plt.show()
+    
+    print(marker01_z)
+    print(labels)
